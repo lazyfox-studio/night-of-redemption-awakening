2f9ff2c2f6288e180e9c35f28066e91f9a7ecedd
vlasov-unn <vla2000@inbox.ru>
2019-03-29 21:04:40 +0300

    view + перемещение + контроль выхода

--- game/main.cpp
+++ game/main.cpp
@@ -10,14 +10,17 @@ int main()
 	sf::Texture bgtexture;
 	if (!bgtexture.loadFromFile("Textures/map.png"))
 	{
-		std::cout << "Cant open" << std::flush;
+		std::cout << "Cant open";
 	};
 	background.setTexture(bgtexture);
 	background.setPosition(sf::Vector2f(0, 0));
+	float vc_x = 1280.f / 2.f, vc_y = 720.f / 2.f;
+
+	view.setCenter(vc_x, vc_y);
+	view.zoom(0.5f);
 
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
-
 	window.setView(view);
 	while (window.isOpen())
 	{
@@ -26,6 +29,29 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::KeyPressed)
+			{
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Key::Right:
+					if(view.getCenter().x < 1280)
+						view.move(10, 0);
+					break;
+				case sf::Keyboard::Key::Left:
+					if (view.getCenter().x > 640)
+						view.move(-10, 0);
+					break;
+				case sf::Keyboard::Key::Up:
+					if (view.getCenter().y > 360)
+						view.move(0, -10);
+					break;
+				case sf::Keyboard::Key::Down:
+					if (view.getCenter().y < 720)
+						view.move(0, 10);
+					break;
+				}
+				window.setView(view);
+			}
 		}
 
 		window.clear();
