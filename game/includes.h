f4a33ac70b9c6c12af845ba4e48f79edeae54e29
MIkhail-Zoreev <mzoreev@gmail.com>
2019-03-29 17:29:34 +0300

    Обновлён includes

--- game/includes.h
+++ game/includes.h
@@ -2,10 +2,14 @@
 
 #include "Headers/Enemy.h"
 #include "Headers/Item.h"
-#include "Headers/Person.h"
+#include "Headers/Ally.h"
 
 // Enemies
 #include "Headers/Enemies/Enemy1.h"
 
 // Items
 #include "Headers/Items/Item1.h"
+
+// Allies
+#include"Headers\Allies\Person.h"
+#include"Headers\Allies\Monolith.h"
