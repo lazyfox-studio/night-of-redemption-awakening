e71870c70d3592a19bbd44b8c56892fd6ce9644a
vlasov-unn <vla2000@inbox.ru>
2019-04-01 18:32:39 +0300

    повороты по диагонали

--- game/Utilities/Controls.h
+++ game/Utilities/Controls.h
@@ -21,34 +21,57 @@ struct CameraOffset
 
 inline void move_all(Person* player)
 {
+	int bitmask = 0;
 	if (Kb.W)
 	{
+		bitmask |= 1;
 		player->move(0.f, -1.0f, Kb.LShift);
-		player->rotate_to(0);
 	}
 	if (Kb.A)
 	{
+		bitmask |= 4;
 		player->move(-1.f, 0.f, Kb.LShift);
-		player->rotate_to(270);
 	}
 	if (Kb.S)
 	{
+		bitmask |= 2;
 		player->move(0.f, 1.0f, Kb.LShift);
-		player->rotate_to(180);
-	}
-	if (Kb.D)
-	{
-		player->move(1.0f, 0.0f, Kb.LShift);
-		player->rotate_to(90);
-	}
-	if (Kb.Q)
-	{
-		player->rotate(-1.0f);
 	}
 	if (Kb.E)
 	{
-		player->rotate(1.0f);
+		player->rotate(45.0f);
 	}
+	if (!bitmask)
+		return;
+	int deg = 0;
+	switch (bitmask)
+	{
+	case 1:
+		deg = 0;
+		break;
+	case 2:
+		deg = 180;
+		break;
+	case 4:
+		deg = 270;
+		break;
+	case 8:
+		deg = 90;
+		break;
+	case 5:
+		deg = 315;
+		break;
+	case 9:
+		deg = 45;
+		break;
+	case 6:
+		deg = 225;
+		break;
+	case 10:
+		deg = 135;
+		break;
+	}
+	player->rotate_to((float)deg);
 }
 
 inline void draw_all(Unit* units[], int count)
