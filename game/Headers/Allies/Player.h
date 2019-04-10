cfbe647882bea85e3621ca79d3d65272e78c48eb
MIkhail-Zoreev <mzoreev@gmail.com>
2019-04-10 16:50:02 +0300

    Sound

--- game/Headers/Allies/Player.h
+++ game/Headers/Allies/Player.h
@@ -6,11 +6,18 @@
 
 #define MAX_STAMINA 120
 
+struct PlayerSound
+{
+    sf::SoundBuffer shoot;
+    sf::SoundBuffer reload;
+};
+
 class Player : public Ally
 {
 	int damage;
     int ammo;
     int stamina;
+    PlayerSound player_sound;
 public:
 	Player();
 	~Player();
--- game/definitions.h
+++ game/definitions.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "Utilities/Screen.h"
 #include "Headers\Allies\Monolith.h"
 
 sf::RenderWindow window(sf::VideoMode(screen.w, screen.h), "Evil Redemption");
 sf::View view;
 Monolith monolith;
+sf::Sound sound;
