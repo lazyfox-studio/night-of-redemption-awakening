cadd8bcffca119015fbe22d23587ddfcfaa426ea
MIkhail-Zoreev <mzoreev@gmail.com>
2019-04-11 19:25:03 +0300

    Коллизии для врагов

--- game/Classes/Enemy.cpp
+++ game/Classes/Enemy.cpp
@@ -45,26 +45,26 @@ void Enemy::move(List<Unit>& units)
     if (r > UNIT_SIZE)
     {
         bool x_unlock = true, y_unlock = true;
+        bool x_left = false, x_right = false, y_left = false, y_right = false, x_space = false, y_space = false;
         float c = speed / r; //Коэфицент подобия
         float dx = (focus->x - x) * c;
         float dy = (focus->y - y) * c;
         for (ListItem<Unit>* i = units.head; i; i = i->next)
         {
-            if (
-                ((x + dx + UNIT_SIZE > i->value->getX()) && (x + UNIT_SIZE < i->value->getX()) && (y < i->value->getY() + UNIT_SIZE / 2) && (y > i->value->getY() - UNIT_SIZE / 2))
-                ||
-                ((x + dx - UNIT_SIZE < i->value->getX()) && (x - UNIT_SIZE > i->value->getX()) && (y < i->value->getY() + UNIT_SIZE / 2) && (y > i->value->getY() - UNIT_SIZE / 2))
-                )
+            //Всевозможные варианты пересечения
+            if ((x + dx + UNIT_SIZE > i->value->getX()) && (x + UNIT_SIZE < i->value->getX())) x_left = true;
+            if ((x + dx - UNIT_SIZE < i->value->getX()) && (x - UNIT_SIZE > i->value->getX())) x_right = true;
+            if ((y + dy + UNIT_SIZE > i->value->getY()) && (y + UNIT_SIZE < i->value->getY())) y_left = true;
+            if ((y + dy - UNIT_SIZE < i->value->getY()) && (y - UNIT_SIZE > i->value->getY())) y_right = true;
+            if ((x < i->value->getX() + UNIT_SIZE / 2) && (x > i->value->getX() - UNIT_SIZE / 2)) x_space = true;
+            if ((y < i->value->getY() + UNIT_SIZE / 2) && (y > i->value->getY() - UNIT_SIZE / 2)) y_space = true;
+            //Проверка на входы
+            if ((x_left || x_right) && y_space) x_unlock = false;
+            if ((y_left || y_right) && x_space) y_unlock = false;
+            if ((x_left || x_right) && (y_right || y_left))
             {
                 x_unlock = false;
-            }
-            if (
-                ((y + dy + UNIT_SIZE > i->value->getY()) && (y + UNIT_SIZE < i->value->getY()) && (x < i->value->getX() + UNIT_SIZE / 2) && (x > i->value->getX() - UNIT_SIZE / 2))
-                ||
-                ((y + dy - UNIT_SIZE < i->value->getY()) && (y - UNIT_SIZE > i->value->getY()) && (x < i->value->getX() + UNIT_SIZE / 2) && (x > i->value->getX() - UNIT_SIZE / 2))
-                )
-            {
-                y_unlock = false;
+                y_unlock = true;
             }
         }
         if (x_unlock == true) x += dx;
