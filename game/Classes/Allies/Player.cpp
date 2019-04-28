17e311daeeee366384e3718b44c7937607a5adc1
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2019-04-28 18:04:09 +0300

    убран дебаг

--- game/Classes/Allies/Player.cpp
+++ game/Classes/Allies/Player.cpp
@@ -1,5 +1,4 @@
 ﻿#include "../../Headers/Allies/Player.h"
-#include <iostream>
 
 Player::Player() : 
 	Ally((float)map.w / 2.0f, (float)map.h / 2.0f + UNIT_SIZE, max_health, 4.0f, 0), damage(80)
@@ -33,7 +32,6 @@ void Player::shoot(List<Enemy> & enemies) {
     float range = float(INT_MAX);
 	float _x, _y;
     float angle = 3.141592f * pov / 180.f;
-	std::cout << pov << "	" << angle << std::endl;
     for (ListItem<Enemy>* i = enemies.head; i; i = i->next)
     {
         if (i->value->r < range) {
@@ -51,7 +49,6 @@ void Player::shoot(List<Enemy> & enemies) {
 					(_y >= y)
                     )
                 {
-					std::cout << "1" << std::endl;
                     target = i->value;
                     range = target->r;
                 }
@@ -68,7 +65,6 @@ void Player::shoot(List<Enemy> & enemies) {
 					(_y >= y)
                     )
                 {
-					std::cout << "2" << std::endl;
                     target = i->value;
                     range = target->r;
                 }
@@ -85,7 +81,6 @@ void Player::shoot(List<Enemy> & enemies) {
 					(_y <= y)
 					)
 				{
-					std::cout << "3" << std::endl;
 					target = i->value;
 					range = target->r;
 				}
@@ -102,7 +97,6 @@ void Player::shoot(List<Enemy> & enemies) {
 					(_y <= y)
 					)
 				{
-					std::cout << "4" << std::endl;
 					target = i->value;
 					range = target->r;
 				}
@@ -113,7 +107,6 @@ void Player::shoot(List<Enemy> & enemies) {
                 {
                     if (((_x - UNIT_SIZE / 2) < x) && ((_x + UNIT_SIZE / 2) > x))
                     {
-						std::cout << "5" << std::endl;
                         target = i->value;
                         range = target->r;
                     }
@@ -125,7 +118,6 @@ void Player::shoot(List<Enemy> & enemies) {
                 {
                     if (((_x - UNIT_SIZE / 2) < x) && ((_x + UNIT_SIZE / 2) > x))
                     {
-						std::cout << "6" << std::endl;
                         target = i->value;
                         range = target->r;
                     }
